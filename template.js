f6fc75949a989b1a53b38f7ed8fe4186afeaa4b4
Luis Calleja <luiscallejaenrique@gmail.com>
2026-02-05 10:35:54 -0300

    feat: create new example tests

--- template.js
+++ template.js
@@ -1,16 +1,49 @@
-// --- import ---
+// --- imports ---
 import test from 'node:test';
 import assert from 'node:assert';
 
-// --- code
+// --- code ---
 
 function sumTwoNumbers(a, b) {
     if ((typeof a !== 'number') || (typeof b !== 'number')) {
-        return new Error('Both numbers most be numbers');
+        throw new TypeError('Not valid numbers.');
     }
 
     return a + b;
 }
 
-console.log(sumTwoNumbers(2, 2))
-console.log(sumTwoNumbers(2, '2'))
+// --- tests ---
+
+test('sumTwoNumbers must return true when both parameters are numbers', () => {
+    assert.ok(sumTwoNumbers(2, 2));
+});
+
+test('sumTwoNumbers must return the sum of 2 + 2', () => {
+    assert.strictEqual(sumTwoNumbers(2, 2), 4);
+});
+
+test('sumTwoNumbers must return a type error when a non-integer value is given', () => {
+    assert.throws(
+        () => sumTwoNumbers(4, '6'),
+        {
+            name: 'TypeError',
+            message: 'Not valid numbers.'
+        }
+    );
+});
+
+// --- other tests ---
+
+function getConfig() {
+    return {
+        mode: 'production',
+        debug: false
+    };
+};
+
+test('Must return current configuration', () => {
+    assert.deepStrictEqual(
+        getConfig(),
+        { mode: 'production', debug: false }
+    );
+});

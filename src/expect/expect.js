0a20a08763680993e3e6d8fb4231ba61c14df7cd
Luis Calleja <luiscallejaenrique@gmail.com>
2026-02-05 11:16:13 -0300

    feat: improve expect.js by using toBe and toEqual functions

--- src/expect/expect.js
+++ src/expect/expect.js
@@ -1,7 +1,13 @@
-// import { toBe, toEqual } from "./matchers.js";
+import { toBe, toEqual } from './matchers.js';
 
 export function expect(received) {
     return {
-        
-    }
+        toBe(expected) {
+            toBe(received, expected);
+        },
+
+        toEqual(expected) {
+            toEqual(received, expected);
+        }
+    };
 }

e54db9591772245f85129bcad23da26fde4875af
Luis Calleja <luiscallejaenrique@gmail.com>
2026-02-06 10:05:21 -0300

    feat: improve runner() function - Implement a try/catch statement to simulate successful and unsuccessful tests. - Add a comment explaining the usage of the for of loop.

--- src/core/runner.js
+++ src/core/runner.js
@@ -5,7 +5,14 @@ export async function runner() {
     const tests = getTests();
 
     for (const test of tests) {
-        return test;
+    // Use a for...of loop instead of Promise.all() as it runs each test in a parallel way without knowing which test goes first 
+        try {
+            await test.fn();
+            console.log(`✔️ ${test.description}`);
+        } catch (e){
+            console.log(`X ${test.description}`);
+            console.error(e.message);
+        }
     };
 };
 

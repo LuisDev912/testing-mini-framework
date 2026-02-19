30066045cc5233c97e5bdad43b3e0bc02b2d4f8e
Luis Calleja <luiscallejaenrique@gmail.com>
2026-02-19 10:24:32 -0300

    test: run the suite tests temporary - Storage the tests in tests.suites to run verify it runs correctly. - Rename "tests" to "allTests" in the runner to improve code readability.

--- src/core/runner.js
+++ src/core/runner.js
@@ -2,14 +2,29 @@ import { getRootTests } from "./test.js";
 
 // the runner function must run each of the tests' function using a try/catch statement
 export async function runner() {
-    const tests = getRootTests();
+    const allTests = getRootTests();
     let passTests = 0;
     let failedTests = 0;
 
     console.time('testsDuration')
-    for (const test in tests) { // currently, it only returns "test.fn is not a funcion" because it doesn't detect any function
-        console.time('test-duration');
-        // Use a for...in loop instead of Promise.all() as it runs each test in a parallel way without knowing which test goes first 
+    // for (const test of allTests.tests) {
+    //     console.time('test-duration');
+    //     // Use a for...in loop instead of Promise.all() as it runs each test in a parallel way without knowing which test goes first 
+    //     try {
+    //         await test.fn();
+    //         console.log(`\u2714 ${test.description}`);
+    //         passTests++;
+    //     } catch (e) {
+    //         console.log(`\u0058 ${test.description}`);
+    //         console.error(e.message);
+    //         failedTests++;
+    //     };
+    //     console.timeEnd('test-duration');
+    // };
+
+    for (const test of allTests.suites) {
+        // temporary test the suites runner
+        console.time('test-duration'); 
         try {
             await test.fn();
             console.log(`\u2714 ${test.description}`);
@@ -23,7 +38,7 @@ export async function runner() {
     };
 
     console.group('\n --- Tests Information ---');
-    console.info(`\u0069 tests: ${tests.length}`);
+    console.info(`\u0069 tests: ${allTests.length}`);
     console.info(`\u0069 pass: ${passTests}`);
     console.info(`\u0069 fail: ${failedTests}`)
     console.timeEnd('testsDuration');
--- src/core/test.js
+++ src/core/test.js
@@ -5,7 +5,7 @@ let rootTests = {
 };
 
 export function test(description, fn) {
-    rootTests.tests.push({ description, fn });
+    rootTests.suites.push({ description, fn });
     // Writing "() => fn" will not work here as the fn parameter is already a function
 };
 

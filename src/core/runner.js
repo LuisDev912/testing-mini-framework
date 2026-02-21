b88335dc67ae698aa0b7fa20255daffd44efecd3
Luis Calleja <luiscallejaenrique@gmail.com>
2026-02-21 12:21:02 -0300

    feat(refactor):improve runner function to show the tests information

--- src/core/runner.js
+++ src/core/runner.js
@@ -22,7 +22,7 @@ async function runSingleTest(test, results) {
 
 async function runSuite(suite, results) {
     if (suite.description !== 'root') {
-        console.group(`\n→ ${suite.description}`);
+        console.group(`\n \u2192 ${suite.description}`);
     }
 
     await runTests(suite.tests, results);
@@ -36,62 +36,23 @@ async function runSuite(suite, results) {
     }
 }
 
-// the runner function must run each of the tests' function using a try/catch statement
 export async function runner() {
     const allTests = getRootTests();
-    let passTests = 0;
-    let failedTests = 0;
 
-    console.time('testsDuration')
-    for (const test of allTests.tests) {
-        console.time('test-duration');
-        // Use a for...in loop instead of Promise.all() as it runs each test in a parallel way without knowing which test goes first 
-        try {
-            await test.fn();
-            console.log(`\u2714 ${test.description}`);
-            passTests++;
-        } catch (e) {
-            console.log(`\u0058 ${test.description}`);
-            console.error(e.message);
-            failedTests++;
-        };
-        console.timeEnd('test-duration');
+    const results = {
+        passed: 0,
+        failed: 0
     };
 
-    async function runSuite(suite) {
-        if (suite.description !== 'root') {
-            console.group(`\n→ ${suite.description}`);
-        }
-
-        for (const test of suite.tests) {
-            try {
-                await test.fn();
-                console.log(`\u2714 ${test.description}`);
-                passTests++;
-            } catch (e) {
-                console.log(`\u0058 ${test.description}`);
-                console.error(e.message);
-                failedTests++;
-            }
-        }
-
-        for (const childSuite of suite.suites) {
-            await runSuite(childSuite);
-        }
-
-        if (suite.description !== 'root') {
-            console.groupEnd();
-        }
-    }
-
-    await runSuite(allTests);
+    console.time('testsDuration')
+    await runSuite(allTests, results);
 
     console.group('\n --- Tests Information ---');
     console.info(`\u0069 tests: ${allTests.tests.length + allTests.suites.length - 1}`);
     // subtract -1 to the final result because it counts "describe()" as a test instead of a group of tests
     console.info(`\u0069 suites: ${allTests.suites.length}`);
-    console.info(`\u0069 pass: ${passTests}`);
-    console.info(`\u0069 fail: ${failedTests}`)
+    console.info(`\u0069 pass: ${results.passed}`);
+    console.info(`\u0069 fail: ${results.failed}`)
     console.timeEnd('testsDuration');
     console.groupEnd();
     console.log('\n')

f5badb33353f51b518f783251656bb72fe79537b
Luis Calleja <luiscallejaenrique@gmail.com>
2026-02-21 12:40:24 -0300

    fix: show the real amount of tests in the tests information

--- src/core/runner.js
+++ src/core/runner.js
@@ -44,16 +44,17 @@ export async function runner() {
         failed: 0
     };
 
-    console.time('testsDuration')
+    console.time('testsDuration');
     await runSuite(allTests, results);
 
     console.group('\n --- Tests Information ---');
-    console.info(`\u0069 tests: ${allTests.tests.length + allTests.suites.length - 1}`);
-    // subtract -1 to the final result because it counts "describe()" as a test instead of a group of tests
+    console.info(`\u0069 tests: ${results.passed + results.failed}`);
+    // sum the passed and failed tests instead of "allTests.tests.length + allTests.suites.length - 1" because it only returned "allTests.tests.length". With this, we can show the real amount of tests
+
     console.info(`\u0069 suites: ${allTests.suites.length}`);
     console.info(`\u0069 pass: ${results.passed}`);
-    console.info(`\u0069 fail: ${results.failed}`)
+    console.info(`\u0069 fail: ${results.failed}`);
     console.timeEnd('testsDuration');
     console.groupEnd();
-    console.log('\n')
+    console.log('\n');
 };

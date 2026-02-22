6ee6888a824632fb2bfa6435940fcb79f6ae2d56
Luis Calleja <luiscallejaenrique@gmail.com>
2026-02-22 10:34:33 -0300

    feat(refactor): improve readability - Change "--async" flag to "--parallel" to avoid ambiguity. - List the tests to show the difference between normal tests and parallel tests.

--- src/core/index.js
+++ src/core/index.js
@@ -1,6 +1,6 @@
 import { runner } from './runner.js';
 
 const args = process.argv.slice(2);
-const isParallel = args.includes('--async');
+const isParallel = args.includes('--parallel');
 
 runner({ parallel: isParallel });
--- src/tests/example.test.js
+++ src/tests/example.test.js
@@ -3,7 +3,7 @@ import { test, describe } from '../core/test.js';
 import { runner } from '../core/runner.js';
 
 // --- toBe() test ---
-test('The result of 2 + 2 must be 4', () => {
+test('(1) The result of 2 + 2 must be 4', () => {
     expect(2 + 2).toBe(4);
     // note: "toBe()" is not imported here as it is a part of "expect()" function
 });
@@ -19,7 +19,7 @@ const User = {
     id: 2
 };
 
-test('must return User information with user role', () => {
+test('(2) must return User information with user role', () => {
     expect(User).toEqual(UserConfig);
 });
 
@@ -32,7 +32,7 @@ function sumTwoNumbers(a, b) {
     return a + b;
 };
 
-test('sumTwoNumbers must throw an error if a parameter is not valid', () => {
+test('(3) sumTwoNumbers must throw an error if a parameter is not valid', () => {
     expect(() => sumTwoNumbers(2, '2')).toThrow();
 });
 // --- toReject() test ---
@@ -45,26 +45,26 @@ async function asyncSum(a, b) {
     return a + b;
 };
 
-test('asyncSum must throw an error if a parameter is not valid', () => {
+test('(4) asyncSum must throw an error if a parameter is not valid', () => {
     expect(() => asyncSum(2, '2')).toReject();
 });
 
 // --- .not() tests ---
-test('not.toBe works', () => {
+test('(5) not.toBe works', () => {
     expect(2 + 2).not.toBe(5);
 });
 
-test('not.toThrow works', () => {
+test('(6) not.toThrow works', () => {
     expect(() => sumTwoNumbers(2, 2)).not.toThrow();
 });
 
 // --- describe() tests ---
-describe('sum', () => {
-    test('The result of 2 + 1 must be 3', () => {
+describe('(suite) sum', () => {
+    test('(7) The result of 2 + 1 must be 3', () => {
         expect(2 + 1).toBe(3);
     });
 
-    test('The result of 2 + 4 must be 5', () => {
+    test('(8) The result of 2 + 4 must be 5', () => {
         expect(2 + 3).toBe(5);
     });
 });

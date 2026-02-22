f123af5740d98554a33198edd96a3c5757f750f0
Luis Calleja <luiscallejaenrique@gmail.com>
2026-02-22 10:23:12 -0300

    feat: add temporal debug logs

--- src/core/index.js
+++ src/core/index.js
@@ -1,3 +1,11 @@
 import { runner } from './runner.js';
 
 const args = process.argv.slice(2);
+const isParallel = args.includes('--async');
+
+console.group('debug');
+console.log(args);
+console.log(isParallel);
+console.groupEnd();
+
+runner({ parallel: isParallel });
